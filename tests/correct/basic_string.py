2a655f904d23331813f800c7465e3dd9e188f420
Kevin Wu <maplefun@gmail.com>
2013-10-12 21:40:19 -0700

    Almost done with strings!

--- tests/correct/basic_string.py
+++ tests/correct/basic_string.py
@@ -8,3 +8,14 @@ print 'wenson ' 'makes strange tests, ' "and is not human"
 print 'God once said \'' "\"\"\'" + " (:)> "
 print """hello
 world"""
+print r"HELLO WORLD"
+print r'tes\ti\ng'
+print r' <(:)'
+print r'\'\''
+print r'a' + 'b'
+print r"an app\le " r"is\ a tree"
+print r'wenson ' r'makes s\trange tests, ' "and \is n\ot human"
+print r'God once said \'' "\"\"\'" + " (:)> "
+print r"""hello
+world"""
+print r'hello' + "world" r'this' + "is another" + r'string' + r'test'
