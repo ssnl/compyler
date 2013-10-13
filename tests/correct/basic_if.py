da7c2dcc8b3b2d02a2984039f34803f4afba585b
whsieh <whsieh@berkeley.edu>
2013-10-13 12:45:35 -0700

    added basic if tests

---
+++ tests/correct/basic_if.py
@@ -0,0 +1,49 @@
+if 2 is 3:
+	print "2 is 3"
+elif 1 is 1:
+	print "1 is 1"
+else:
+	print "2 is not 3 and 1 is not 1"
+
+if 1 is 1: print "1 is 1"
+elif 2 is 1:
+	print "2 is 1"
+else:
+	print "1 is not 1 and 2 is not 1"
+
+if 1 is 4:
+	print "1 is 4"
+elif 2 is 2: print "2 is 2"
+else:
+	print "1 is not 4 and 2 is not 2"
+
+if 9 is 4:
+	print "9 is 4"
+elif 1 is 3: print "1 is 3"
+else: print "9 is not 4 and 1 is not 3"
+
+if 5 is 1: print "5 is 1"
+elif 2 is 0: print "2 is 0"
+else: print "5 is not 1 and 2 is not 0"
+
+if ():
+	print "1"
+elif []: 
+	print "2"
+elif [1,2,3]:
+	print [1,2,3]
+else: print "5 is not 1 and 2 is not 0"
+
+if True:
+	print "yep"
+	if False:
+		print "no"
+		if True:
+			print "huh?"
+		elif False: print "i don\"t even"
+	elif True:
+		print "indeed"
+	else:
+		print "wat"
+else:
+	print "nope"
