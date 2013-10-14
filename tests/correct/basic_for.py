cc5fd7338525840db5096cd2a593b92fa5ca012c
whsieh <whsieh@berkeley.edu>
2013-10-13 21:34:06 -0700

    for and while loops

---
+++ tests/correct/basic_for.py
@@ -0,0 +1,10 @@
+for i in [1,2,3]:
+	print "first"
+	print i
+
+for j in (0,4,8):
+	print "second"
+	print j
+else:
+	print "done!"
+	
---
+++ tests/correct/basic_while.py
@@ -0,0 +1,13 @@
+i = 0
+while i < 5:
+	print i
+	i = i + 1
+print "done"
+
+j = 9
+while j > 0:
+	print j
+	j = j - 1
+else:
+	print "done: "
+	print j
