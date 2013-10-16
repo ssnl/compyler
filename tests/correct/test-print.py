f5df98e703dbc9525f470b73b0025bdc47c0ed68
Leo Kam <leohtkam@gmail.com>
2013-10-16 04:21:43 -0700

    Fixed tuple, can now match X, as (X,)

---
+++ tests/correct/test-print.py
@@ -0,0 +1,9 @@
+print
+print "Line1"
+print "Line2",
+print "Line2a", "Line2b"
+print "Line3a", "Line3b",
+print >> sys.stdout
+print >> sys.stdout, "Line4"
+print >> sys.stdout, "Line5",
+print "End"
---
+++ tests/correct/test-tuples.py
@@ -0,0 +1,15 @@
+x = 1,
+print x
+x = 1, 2, 3
+print x
+x = (1,)
+print x
+x = (1, 2, 3); print x; x = (1, (2, 3), 4); print x
+x = ((1, 2), (3, 4))
+print x
+x = ()
+print x
+x = (1)  # Not a tuple
+print x
+x = 1, 2,
+print x
