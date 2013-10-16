9c91fbe51375e79d32a696071d8d971ea8d163bc
Kevin Wu <maplefun@gmail.com>
2013-10-15 22:50:01 -0700

    added passes

---
+++ tests/correct/basic_pass.py
@@ -0,0 +1,33 @@
+def testfun1():
+  pass
+  x = 10
+  pass
+  return x
+  pass
+
+def testfun2():
+  pass
+
+class testclass1:
+  pass
+
+if False:
+  pass
+
+if True:
+  pass
+
+while 1 == 0:
+  pass
+
+for i in range(1,2):
+  pass
+  if 2 == 1:
+    pass
+
+pass
+pass
+x = 1
+pass
+print x
+
