94f47475ec7ef4751f2d002fed23381792fd9bdb
whsieh <whsieh@berkeley.edu>
2013-12-13 10:01:37 -0800

    added tests

---
+++ tests/correct3/alloc.py
@@ -0,0 +1,5 @@
+class Foo of [$T]:
+    def __init__(self, a::int, b::int):
+        pass
+a = Foo of int(1,2)
+
---
+++ tests/correct3/while.py
@@ -0,0 +1,11 @@
+x = 1
+while x < 2:
+    print "pass"
+    x = 3
+else:
+    print "fail"
+
+while x < 0:
+    print "fail"
+else:
+    print "pass"
